04e2d5a6de6a9381ceafa27f23430591ca6ac4b0
davidjeddy <me@davidjeddy.com>
2022-01-30 16:49:49 +0100

    ADDED name to SG

--- sg.tf
+++ sg.tf
@@ -1,9 +1,12 @@
 resource aws_security_group this {
   description = "TCP egress and ingress rules."
+  name        = join(var.delimiter, [var.name, var.stage, random_string.this.id])
   vpc_id      = var.vpc_id
 
   tags = merge(
     var.tags,
-    { Name = join(var.delimiter, [var.name, var.stage, random_string.this.id]) }
+    {
+      Name = join(var.delimiter, [var.name, var.stage, random_string.this.id])
+    }
   )
 }
